a7abf6ca18613453c5a28aae5dce7b7388f6b547
ivori <ivori.koyotgmail.com>
2016-09-23 15:52:55 +0200

    tp AIOP

---
+++ src/fr/polytech/todo/servlet/HelloServlet.java
@@ -0,0 +1,53 @@
+package fr.polytech.todo.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/Hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		ServletOutputStream out = response.getOutputStream();
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<tilte>Hello Page</title>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<h1>Hello "+request.getParameter("name")+"</h1>");
+		out.print("</body>");
+		out.print("<head>");
+		out.print("</html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
